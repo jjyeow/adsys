3049cb49bc4dc69821dd46e3f66edb9b2ac30e34
jjyeow <yeowjingjong@gmail.com>
2023-02-11 20:33:41 +0800

    b

--- component/partner.jsx
+++ component/partner.jsx
@@ -6,7 +6,7 @@ function Partner(props) {
         <div>
             <div style={{width: props.width ? "40vw" : "80vw"}}>
                 <div style={{display: "flex", justifyContent: "flex-start", flexDirection: props.width ? "row" : "column", paddingBottom: 50}}>
-                    <div style={{width: "50%", marginRight: props.width ? 50 : 0, borderRadius: '50%'}}>
+                    <div style={{width: props.width ? "100%" : "50%", marginRight: props.width ? 50 : 0, borderRadius: '50%'}}>
                         <Image src={props.avatar} height={100} width={100}/>
                     </div>
                     <div style={{textAlign: "left", lineHeight: "1.5em", marginTop: props.width ? 0 : 30, fontSize: props.width ? "1.5rem" : "1.1rem"}}>
